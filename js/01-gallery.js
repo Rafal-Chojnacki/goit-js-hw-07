2e616b755ab86e3ffd178be9e039bd42c26339ee
Rafał Chojnacki <r.chojnacki2@o2.pl>
2023-04-24 19:33:58 +0200

    HW07 first part

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery')
+const items = []
+for (let i = 0; i < galleryItems.length; i++) {
+  const item = document.createElement('li')
+  item.classList.add('gallery__item')
+  item.insertAdjacentHTML("beforeend", `<img data-id="${i}" class = "gallery__image" src="${galleryItems[i].preview}" alt="${galleryItems[i].description}" data-src="${galleryItems[i].original}" width = 372px height = 240px >` );
+  items.push(item)
+};
+gallery.append(...items)
+
+gallery.addEventListener('click', function(e) {
+  
+  if (e.target.tagName === 'IMG') {
+    const imgUrl = e.target.getAttribute('data-src');
+    console.log(imgUrl)
+    const instance = basicLightbox.create(`<img src="${imgUrl}" width="100%">`,
+    {
+      onShow: (instance) => {
+        document.addEventListener('keydown', e => {
+          if (e.keyCode !== 27) { return; }
+          
+          instance.close(); 
+        });
+      },
+      onClose: (instance) => {
+        document.removeEventListener('keydown', e => {
+          if (e.keyCode !== 27) { return; }
+          instance.close(); 
+        });
+      }
+    });
+    instance.show()
+  }
+});	
+
