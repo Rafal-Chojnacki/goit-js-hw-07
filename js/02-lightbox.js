0032daf95a8b2ac76276c68433baffe0d31ac2dc
Rafał Chojnacki <r.chojnacki2@o2.pl>
2023-04-24 19:56:48 +0200

    HW7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,14 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery')
+const items = []
+for (let i = 0; i < galleryItems.length; i++) {
+  const item = document.createElement('li')
+  item.classList.add('gallery__item')
+  item.insertAdjacentHTML("beforeend", `<a class="gallery__item" href="${galleryItems[i].original}">
+  <img class="gallery__image" src="${galleryItems[i].preview}" alt="${galleryItems[i].description}" />
+</a>` );
+  items.push(item)
+};
+gallery.append(...items)
+let lightbox = new SimpleLightbox('.gallery a', { /* options */ });
